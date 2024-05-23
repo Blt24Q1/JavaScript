df523cdb09ea677050c57ebaa1ade7ee6c67f79c
Blt24Q1 <blt24q1@gmail.com>
2024-05-23 16:18:57 +0900

    Update 04-boolean.js
    Commit Unsaved Contents

--- 04-boolean.js
+++ 04-boolean.js
@@ -24,7 +24,10 @@ console.log(123 === "123"); //  false -> number, string -> 값과 타입을 함�
 console.log(Boolean("JavaScript"), Boolean(""));    //  string
 console.log(Boolean(2024), Boolean(0));             //  number
 
-
-
+//  Short-Curcuit 선택 기법
+//  Front-End 선택적 렌더링
 console.log(true && "Test");    //  Test
 console.log(false && "Test");   //  false
+
+console.log(true || "Test");    //  true
+console.log(false || "Test");   //  Test
