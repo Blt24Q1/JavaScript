467315d11cd9f8653cb1d8b0c8d2c9e8567a8ab5
Blt24Q1 <blt24q1@gmail.com>
2024-05-24 12:07:51 +0900

    Update 06-function.js

--- 06-function.js
+++ 06-function.js
@@ -39,3 +39,53 @@ function getNumberTotal() {
 }
 console.log(getNumberTotal(1, 2, 3, 4, 5));
 console.log(getNumberTotal(1, 2, "3", 4, 5));
+
+//  함수는 객체 -> 변수에 할당되거나 다른 함수의 매개변수로 전달될 수 있다.
+function calculator(a, b, func) {   //  func -> 계산식 함수
+    if (typeof func === "function") {
+        console.log(func(a, b));
+    } else {
+        console.log("어쩌라구");
+    }
+}
+
+calculator(10, 20);
+calculator(10, 20, function(v1, v2) {
+    return v1 + v2;
+});
+
+//  함수 보충
+console.log("=================== 화살표 표기법");   //  매개변수 목록 => 구현 내용
+
+const f1 = function() {
+    return "hello";
+}   //  매개변수 x, 리턴 O
+console.log(f1());
+const f1_arrow = () => "hello";
+console.log(f1_arrow());
+
+const f2 = function(name) {
+    return "Hello, " + name;
+}   //  매개변수 O, 리턴 O
+console.log(f2("홍길동"));
+
+// const f2_arrow = (name) => "Hello, " + name;
+const f2_arrow = name => "Hello, " + name;
+console.log(f2_arrow("홍길동"));
+
+const f3 = function(a, b) {
+    return a + b;
+}
+console.log(f3(10, 20));
+const f3_arrow = (a, b) => {    
+    //  여러 개의 로직 수행할 때 -> 블록({})을 잡을 수 있다
+    //  블록이 잡히면 return 문을 생략할 수 없다
+    console.log("화살표 함수");
+    return a + b;
+};
+
+//  콜백 함수에 화살표 함수로 계산식 정의
+calculator(10, 20, (v1, v2) => v1 + v2);
+calculator(10, 20, (v1, v2) => v1 * v2);
+
+//  콜백 함수 활용 -> Array 추가 강의
